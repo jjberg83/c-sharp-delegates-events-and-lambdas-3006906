a4d5ab0d36aa49dd2d6cc46dadaf8f21849d2ae2
Jørund Jenssen Berg <jjberg@gmail.com>
2023-06-30 06:27:39 +0200

    Challenge so far

--- Start/Delegates/DelegatesSolution/Program.cs
+++ Start/Delegates/DelegatesSolution/Program.cs
@@ -4,8 +4,31 @@ namespace DelegatesSolution
 {
     class Program
     {
+        static float zone1Fees(float pris) {
+            return pris * 1.25f;
+        }
+        static float zone2Fees(float pris)
+        {
+            return pris * 1.12f + ;
+        }
         static void Main(string[] args)
         {
+            bool loopButton = true;
+            do 
+            {
+                Console.WriteLine("What is the destination zone?");
+                string destinasjon = Console.ReadLine();
+                Console.WriteLine("What is the item price?");
+                string pris = Console.ReadLine(); 
+                float prisFloat = float.Parse(pris);
+                if (destinasjon == "exit") | (pris )
+                {
+                    loopButton = false;
+                }
+            }
+            while (loopButton)
+
+
         }
     }
 }
