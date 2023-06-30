dddbd294a2013c11c5c67ccf09305e1df80159db
Jørund Jenssen Berg <jjberg@gmail.com>
2023-06-30 02:28:28 +0200

    Composable delegates 2, der vi bruker ref

--- Start/Delegates/Composable2/Program.cs
+++ Start/Delegates/Composable2/Program.cs
@@ -3,16 +3,17 @@
 namespace Composable2
 {
     // declare the delegate type
-    public delegate void MyDelegate(int arg1, int arg2);
+    public delegate void MyDelegate(int arg1, ref int arg2);
 
     class Program
     {
-        static void func1(int arg1, int arg2)
+        static void func1(int arg1, ref int arg2)
         {
             string result = (arg1 + arg2).ToString();
+            arg2 += 20;
             Console.WriteLine("The number is: " + result);
         }
-        static void func2(int arg1, int arg2)
+        static void func2(int arg1, ref int arg2)
         {
             string result = (arg1 * arg2).ToString();
             Console.WriteLine("The number is: " + result);
@@ -29,12 +30,12 @@ namespace Composable2
 
             // call each delegate and then the chain
             Console.WriteLine("Calling the first delegate");
-            f1(a, b);
+            f1(a,ref b);
             Console.WriteLine("Calling the second delegate");
-            f2(a, b);
+            f2(a, ref b);
             // Call the composed delegate
             Console.WriteLine("\nCalling the chained delegates");
-            f1f2(a, b);
+            f1f2(a, ref b);
 
         }
     }
