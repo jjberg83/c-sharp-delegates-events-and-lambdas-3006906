9df51f9c9879d6414485fe6b08439deb7ca1b4c6
Jørund Jenssen Berg <jjberg@gmail.com>
2023-07-01 04:35:07 +0200

    Basic events

--- Start/Events/BasicEvents/Program.cs
+++ Start/Events/BasicEvents/Program.cs
@@ -10,12 +10,14 @@ namespace BasicEvents
         private string theVal;
 
         // TODO: declare the event
+        public event myEventHandler valueChanged;
 
         public string Val
         {
             set {
                 this.theVal = value;
                 // TODO: when the value changes, fire the event
+                this.valueChanged(theVal);
 
             }
         }
@@ -27,9 +29,13 @@ namespace BasicEvents
         {
             // TODO: use a named function as an event handler
             EventPublisher obj = new EventPublisher();
+            // obj.valueChanged += new myEventHandler(obj_valueChanged);
 
             // TODO: use an anonymous delegate as an event handler
-
+            obj.valueChanged += delegate (string val)
+            {
+                Console.WriteLine("The Value changed to {0}", val);
+            };
 
             string str;
             do {
