50c0634806eae0c2eda69b067fd0de76f6bd7f55
Jørund Jenssen Berg <jjberg@gmail.com>
2023-07-04 07:12:34 +0200

    Fikk Chained Events tilbake til original

--- Start/Events/ChainedEvents/Program.cs
+++ Start/Events/ChainedEvents/Program.cs
@@ -10,8 +10,7 @@ namespace ChainedEvents
         private string theVal;
         // declare the event handler
         public event myEventHandler valueChanged;
-        // TODO4: Use the EventArgs class
-
+        public event EventHandler<ObjChangeEventArgs> objChanged;
 
         public string Val
         {
@@ -19,13 +18,15 @@ namespace ChainedEvents
                 this.theVal = value;
                 // when the value changes, fire the event
                 this.valueChanged(theVal);
-                // TODO5: Use the custom event handler
-
+                this.objChanged(this, new ObjChangeEventArgs() { propChanged = "Val" });
             }
         }
     }
 
-    // TODO3: Create a subclass of EventArgs for our use
+    class ObjChangeEventArgs : EventArgs
+    {
+        public string propChanged;
+    }
 
     class Program
     {
@@ -33,14 +34,18 @@ namespace ChainedEvents
         {
             // create the test class
             EventPublisher obj = new EventPublisher();
-            // TODO1: Connect multiple event handlers
+            // Connect multiple event handlers
+            obj.valueChanged += new myEventHandler(changeListener1);
+            obj.valueChanged += new myEventHandler(changeListener2);
 
+            // Use an anonymous delegate as the event handler
+            obj.valueChanged += delegate (string s) {
+                Console.WriteLine("This came from the anonymous handler!");
+            };
 
-            // TODO2: Use an anonymous delegate as the event handler
-
-
-            // TODO6: Listen for the custom event we defined with EventArgs
-
+            obj.objChanged += delegate (object sender, ObjChangeEventArgs e) {
+                Console.WriteLine("{0} had the '{1}' property changed", sender.GetType(), e.propChanged);
+            };
 
             string str;
             do {
